4abf18941a5f0b8031e88483d024f7fb5f703daa
psjoshi20 <psjoshi20@gmail.com>
2019-11-07 15:01:08 +0800

    syntax change in js file

--- db/migrations/20190728084151-create-payments.js
+++ db/migrations/20190728084151-create-payments.js
@@ -6,7 +6,7 @@ module.exports = {
         allowNull: false,
         autoIncrement: false,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       description:{
           allowNull:false,
