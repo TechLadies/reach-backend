b77d1dad34b4961c3e705b0bab2bf2f43859a365
Sindhuja Kumaran <sindhuja@Sindhujas-MacBook-Pro-2.local>
2019-12-06 13:02:00 +0800

    - set up passport auth for login with ‘email’ & ‘password’

---
+++ src/auth/passport.js
@@ -0,0 +1,47 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const { User } = require("../models");
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.EXPRESS_SESSION_SECRET,
+};
+
+passport.use('local',
+  new LocalStrategy({
+    usernameField: 'email',
+    passwordField: 'password'
+  }, 
+  async (email, password, done) => {
+    let user = await User.findOne({ where: { email: email } });
+
+    // if user doesnt exist
+    if (!user) {
+    return done(null, false, { message: "User not found." });
+    }
+
+    // authentication
+    user.validatePassword(password, user, done);
+  })
+);
+
+passport.use('jwtauth',
+  new JwtStrategy(jwtOptions, async function ({ userId }, done) {
+    // If no id, just return and let the authenticate middleware deal
+    // with response
+    if (!userId) {
+      return done(null, null);
+    }
+
+    // Deserialise logic from token
+    const user = await User.findByPk(userId);
+    if (user) {
+      return done(null, user);
+    } else {
+      return done({ error: 'User not found.' }, null);
+    }
+  })
+);
+
+module.exports = passport;
