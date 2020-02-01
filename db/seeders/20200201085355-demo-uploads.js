274ce3f1807e7944b4ec54b05ce85e8298f7197b
psjoshi20 <psjoshi20@gmail.com>
2020-02-01 18:08:03 +0800

    created api for uploads

---
+++ db/seeders/20200201085355-demo-uploads.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Uploads', 
+    [
+      {
+        uploadDate: "2019-12-31",
+        firstDate: "2019-01-01",
+        lastDate: "2019-01-31",
+        createdAt: Sequelize.literal('NOW()'),
+        updatedAt: Sequelize.literal('NOW()')
+      },
+      {
+        uploadDate: "2019-11-30",
+        firstDate: "2019-01-01",
+        lastDate: "2019-02-28",
+        createdAt: Sequelize.literal('NOW()'),
+        updatedAt: Sequelize.literal('NOW()')
+      },
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Uploads', null, {});
+  }
+};
--- src/routes/uploads.js
+++ src/routes/uploads.js
@@ -12,4 +12,25 @@ router.get('/', function (req, res, next) {
 
 });
 
+// /latest
+router.get('/latest', function (req, res, next) {
+  
+        db.Upload.findAll({
+            order:[
+              //
+              ['uploadDate', 'DESC'],
+            ]
+        }).then(uploads => {
+    res.json(uploads[0])
+    //      db.Upload.findOne({
+    //         order:[
+    //           //
+    //           [db.sequelize.fn('max', db.sequelize.col('uploadDate')), 'DESC'],
+    //         ]
+    //     }).then(uploads => {
+    // res.json(uploads)
+  });
+        
+});
+
 module.exports = router;
