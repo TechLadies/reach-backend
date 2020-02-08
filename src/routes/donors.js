cfb98805006bb0c250f178f64061919b17433c2f
Ler Ka Leng <kalengzonline@hotmail.com>
2020-02-08 14:52:11 +0800

    add date filter in donor api

--- src/routes/donors.js
+++ src/routes/donors.js
@@ -2,37 +2,96 @@ const express = require("express");
 const router = express.Router();
 const Sequelize = require("sequelize");
 const db = require("../models/index");
+const pagination = require("../middlewares/pagination");
 
 //donor list table
-router.get("/", function(req, res, next) {
-  //Hi all, remind me to explain what try and catch is
+router.get("/", pagination, function(req, res, next) {
+  let offset = req.customParams.offset;
+  let limit = req.customParams.limit;
 
   var donor = db.Donor;
+  const now = new Date();
+  const thisYear = now.getFullYear();
+  const thisMonth = now.getMonth();
+  const thisDate = now.getDate();
+  // const {
+  //   from = new Date(thisYear, 0, 1).toISOString(),
+  //   to = new Date(thisYear, thisMonth, thisDate).toISOString(),
+  //   page = 1
+  // } = req.query;
+
+  //   try {
+  //     const donors = await db.Donor.findAll({
+  //       limit: limit,
+  //       offset: offset,
+  //       order: [["id", "ASC"]]
+  //     });
+  //     res.json({
+  //       data: donors,
+  //       perPage: limit,
+  //       offset: offset
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   }
+  // });
 
   try {
-    donor.findAll({
-      attributes: [
-        "idNo",
-        "name",
-        "contactNo",
-        "email",
-        "dnc",
-        [Sequelize.fn('SUM', Sequelize.col('donationAmount')), 'totalAmountDonated']
-      ],
-      include: [
-        {
-          model: db.Donation,
-          as: "donations",
-          attributes: []
-        }
-      ],
-      group: ['Donor.id']
-    }).then(donorObj => {
-      res.json(donorObj);
-    });
+    donor
+      .findAll({
+        limit: limit,
+        offset: offset,
+        // where (for advanced filters)
+        attributes: [
+          "idNo",
+          "name",
+          "contactNo",
+          "email",
+          "dnc",
+          [
+            Sequelize.fn("SUM", Sequelize.col("donationAmount")),
+            "totalAmountDonated"
+          ]
+        ],
+        include: [
+          {
+            model: db.Donation,
+            as: "donations",
+            // where: {
+            //   donationDate: {
+            //     [Sequelize.Op.between]: [new Date(from), new Date(to)
+            // ]
+            //   }
+            // },
+            // include: [
+            //   {
+            //     model: db.Source
+            //   }
+            // ]
+            attributes: []
+          }
+        ],
+        group: ["Donor.id"],
+        subQuery: false
+      })
+      .then(donorObj => {
+        res.json({
+          data: donorObj,
+          perPage: limit,
+          offset: offset
+        });
+      });
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get("/count", function(req, res, next) {
+  db.Donor.count().then(count => {
+    console.log(count);
+    res.json(count);
+  });
 });
 
 router.get("/sums", function(req, res, next) {
