506b36326be9b0f7c4011ae4c6b507f478e93f5b
chong lu khei <clukhei@gmail.com>
2020-04-03 09:43:07 +0800

    remove comments from migration file

--- db/migrations/20200401134100-drop_uploadDate_column.js
+++ db/migrations/20200401134100-drop_uploadDate_column.js
@@ -3,23 +3,9 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.removeColumn('Uploads', 'uploadDate')
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
   },
 
   down: (queryInterface, Sequelize) => {
     return queryInterface.addColumn('Uploads', 'uploadDate')
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
   }
 };
