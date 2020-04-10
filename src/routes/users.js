e80f4384cba01671ea2e4ef0a9e0fa2d768cf517
chong lu khei <clukhei@gmail.com>
2020-04-10 15:37:48 +0800

    Reset password part 1
    -add Columns in User table to store token and token expiry Date
    -install cryto lib for generation of randomByte token
    -implement db query and functions to check user exist, generate store and send token
    - using nodemailer and MailTrap temporarily to test send email

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const debug = require('debug')('app:users')
 const db = require('../models/index')
 const nodeMailer = require('nodemailer')
+const crypto = require('crypto')
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -37,38 +38,78 @@ router.get('test/', function (req, res, next) {
     res.json(users)
   })
 })
+module.exports = router
 
 router.post('/send_email', function (req, res, next) {
-  const mailConfig = {
-    host: 'smtp.mailtrap.io',
-    port: 2525,
-    auth: {
-      user: 'e1fa9b8d5ae44b',
-      pass: 'a199dbc739b3a2',
-    },
-  }
-  const transporter = nodeMailer.createTransport(mailConfig)
+  //  user by email
+  db.User.findOne({
+    where: { email: req.body.email }
+  })
+    .then((user) => generateStoreSendToken(user))
+    .catch((err) => console.log(err))
 
-
-  const mailOptions = {
-    from: '"Info" <info@example.com>', // sender address
-
-    to: 'kheiboardwarrior@gmail.com', // list of receivers
-
-    subject: 'test', // Subject line
-
-    text: 'hello world, its my first message sent', // plain text body
-
-    html:
-      '<b>Hey there! </b><br> This is our first message sent with Nodemailer', // html body
-  }
-
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
+  // if user exist, generate and store token in User table
+  function generateStoreSendToken(user) {
+    if (user) {
+      return crypto.randomBytes(127, function (err, buffer) {
+        if (err) throw err
+        const token = buffer.toString('hex')
+        return db.User.update(
+          {
+            resetPasswordToken: token,
+            resetPasswordExpiry: Date.now() + 86400000,
+          },
+          {
+            where: { id: user.id },
+            returning: true
+          }
+        )
+          .then(([, updated]) => {
+            sendEmail(updated[0])
+          })
+          .catch((err) => console.log(err))
+      })
+    } else {
+      user = { value: 'User does not exist' }
     }
-    console.log('Message sent: %s', info.messageId);
-});
-})
+  }
 
-module.exports = router
+  // send email with token to the user
+  function sendEmail(user) {
+    const mailConfig = {
+      host: 'smtp.mailtrap.io',
+      port: 2525,
+      auth: {
+        user: 'e1fa9b8d5ae44b',
+        pass: 'a199dbc739b3a2'
+      },
+    }
+    const transporter = nodeMailer.createTransport(mailConfig)
+    const mailOptions = {
+      from: '"Info" <info@example.com>', // sender address
+
+      to: user.email, // list of receivers
+
+      subject: 'Reset Password', // Subject line
+      context: {
+        url:
+          'http://localhost:3000/reset_password?token=' +
+          user.resetPasswordToken,
+        name: user.firstName
+      },
+      // text: `Dear , you have requested for a password reset. Please click on the password reset link`, // plain text body
+
+      html: `<b>Dear ${user.firstName} </b><br> you have requested for a password reset. Please click on the password reset link : http://localhost:3000/reset_password?token= + ${user.resetPasswordToken}`, // html body
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (!error) {
+        return res.json({
+          message: `${info.messageId} sent! Kindly check your email for further instructions`,
+        })
+      } else {
+        return console.log(error)
+      }
+    })
+  }
+})
