943405ecbde0218cb711255815cb5101b6c98558
Shang <ong.shang@gmail.com>
2020-04-17 17:39:26 +0800

    Removed "UploadDate" from seeding.

--- db/seeders/20200201085355-demo-uploads.js
+++ db/seeders/20200201085355-demo-uploads.js
@@ -5,21 +5,18 @@ module.exports = {
     return queryInterface.bulkInsert('Uploads', 
     [
       {
-        uploadDate: "2019-12-31",
         firstDate: "2019-01-01",
         lastDate: "2019-01-31",
         createdAt: Sequelize.literal('NOW()'),
         updatedAt: Sequelize.literal('NOW()')
       },
       {
-        uploadDate: "2019-11-30",
         firstDate: "2019-01-01",
         lastDate: "2019-02-28",
         createdAt: Sequelize.literal('NOW()'),
         updatedAt: Sequelize.literal('NOW()')
       },
       {
-        uploadDate: "2019-10-30",
         firstDate: "2019-01-01",
         lastDate: "2019-01-01",
         createdAt: Sequelize.literal('NOW()'),
