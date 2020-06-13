1ff53ea691e83b2dfd9bb74734136dfe9542ff7c
Lu Khei <kheiboardwarrior@gmail.com>
2020-06-13 17:22:38 +0800

    change limit to 2mb

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const app = express()
 
 app.use(cors())
 app.use(logger('dev'))
-app.use(express.json({limit: '50mb'}));
+app.use(express.json({limit: '2mb'}));
 app.use(express.urlencoded({ limit: '50mb', extended: false }))
 app.use(cookieParser())
 app.use(passport.initialize());
